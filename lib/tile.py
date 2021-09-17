7bc58a2675d4ab7423fe4a8b9bb16b31cc8aeda8
gvituri <gustavovituri@gmail.com>
2021-09-17 08:49:12 -0300

    Add animated tiles and map-tile integration

--- lib/tile.py
+++ lib/tile.py
@@ -1,3 +1,4 @@
+import random
 class Tile:
 
     color_reset = "\u001b[0m"
@@ -5,10 +6,23 @@ class Tile:
 
     def __init__(self, tile_info):
         self.code = tile_info[0]
-        self.name = tile_info[1]
-        self.color = Tile.color_base + tile_info[2]
-        self.anim_type = tile_info[3]
-        self.character = tile_info[4]
+        self.color = Tile.color_base + tile_info[1]
+        self.animation_type = tile_info[2]
+        self.animation_frame = 0
+        self.sprite_sheet = tile_info[3]
 
     def __str__(self):
-        return '%s%s%s' %(self.color, self.character, Tile.color_reset)
+        return '%s%s%s' %(self.color, self.define_sprite(self.animation_type, self.animation_frame, self.sprite_sheet), Tile.color_reset)
+
+    def define_sprite(self, animation_type, animation_frame, sprite_sheet):
+        if animation_type == "s":
+            return sprite_sheet
+        elif animation_type == "r":
+            return random.choice(list(sprite_sheet))
+        elif animation_type == "a":
+            frame = sprite_sheet[animation_frame]
+            if self.animation_frame == len(sprite_sheet) - 1:
+                self.animation_frame = 0
+            else:
+                self.animation_frame += 1
+            return frame
--- lib/tilesys_test.py
+++ lib/tilesys_test.py
@@ -1,4 +1,6 @@
+from os import system
 from tile import Tile
+import time
 
 map_file = open("./files/tilesys_test.csv", "r")
 map_data = map_file.read()
@@ -23,5 +25,25 @@ tile_palette = []
 for tile in tile_map:
     tile_palette.append(Tile(tile))
 
-for tile in tile_palette:
-    print(tile)
+map_file = open("./files/tilesys_test.csv", "r")
+map_data = map_file.read()
+map_rows = map_data.split("\n")
+raw_map = []
+for row in map_rows:
+    split_row = row.split(",")
+    raw_map.append(split_row)
+
+for row in raw_map:
+    for item in row:
+        print(item, end = "")
+    print()
+
+while True:
+    system("clear")
+    for row in raw_map:
+        for item in row:
+            for char in tile_palette:
+                if item == char.code:
+                    print(char, end = "")
+        print()
+    time.sleep(1)
